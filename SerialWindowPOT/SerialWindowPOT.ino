0b706b7e4f9b0d70240a830c23edd57568d41c50
zselgrath <35938826+zselgrath@users.noreply.github.com>
2018-01-29 23:28:49 -0500

    this is the first one i typed from VS Code

--- SerialWindowPOT/SerialWindowPOT.ino
+++ SerialWindowPOT/SerialWindowPOT.ino
@@ -2,7 +2,7 @@
   TO DO:
   Protect against non integer or out of range values from being entered
     Check for 0<=X<=255
-    Hi
+    Hi lol
 *********************************************************************************************************/
 #include <mcp_can.h>
 #include <SPI.h>
