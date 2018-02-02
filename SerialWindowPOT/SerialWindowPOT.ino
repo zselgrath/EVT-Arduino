a76527137ea6ff23d674f4c0410223281521b3db
zselgrath <35938826+zselgrath@users.noreply.github.com>
2018-02-02 00:55:20 -0500

    WOOOOWWWW

--- SerialWindowPOT/SerialWindowPOT.ino
+++ SerialWindowPOT/SerialWindowPOT.ino
@@ -8,8 +8,6 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
-int sensorPin = A0;    // select the input pin for the potentiometer
-int sensorValue = 0;
 const int SPI_CS_PIN = 9;
 MCP_CAN CAN(SPI_CS_PIN);
 
@@ -23,7 +21,7 @@ void setup() {
     }
   Serial.println("CAN BUS Shield init ok!");
   delay(100);
-  Serial.println("Valid commands: send , edit , address, pot");
+  Serial.println("Valid commands: send , edit , address, continuous");
   delay(100);
 }
 
@@ -33,7 +31,7 @@ String command_str = "";
 boolean editFlag = false;         // user will edit the CAN Message
 boolean addressFlag = false;      // user will edit the receiving CAN Address 
 boolean sendFlag = false;         // user wants the existing CAN message to be sent
-boolean potFlag = false;          // user wants to start pot values in CAN messages to be sent
+boolean continuousFlag = false;   // user wants to continuously send torque values in CAN messages
 int editInt = 0;
 unsigned int CANaddress = 528;
 
@@ -46,18 +44,11 @@ void loop() {
   
   readSerial();
   
-  if(sendFlag){
-    send();
-  }
-  else if(editFlag && command_str != ""){
-    edit();
+  if(editFlag && command_str != ""){
+    editMessage();
   }
   else if(addressFlag && command_str != ""){
-    address();
-  }
-  else if (potFlag && command_str != "")
-  {
-    address();
+    editAddress();
   }
   else{
     setFlag();
@@ -82,31 +73,28 @@ void readSerial(){
 }
 
 void setFlag(){
-  if (command_str == "edit") {
-      //MAKE THIS
-      Serial.println("Enter your message in pairs of bytes (integer values 0-255!!!)");
-      Serial.println("Enter the X value: [X,0,0]");
+    if (command_str == "edit") {
+      Serial.println("Enter the torque value as an integer from 0 to 32767");
       editFlag = true;
+      command_str = "";      
+    }
+    if (command_str == "send") {
+      sendMessage();
       command_str = "";
-  }
-  if (command_str == "send") {
-      sendFlag = true;
+    }
+    if (command_str == "continuous") {
+      Serial.println("continuous is WIP, entering loop");
       command_str = "";
-  }
-  if (command_str == "pot") {
-      //TODO: build a way to pot stored CAN messages by specifying an order and a delay
-      potFlag = true;
-      Serial.println("pot is WIP, reading potentiometer values");
-      command_str = "";
-  }
-  if (command_str == "address") {
+      continuous();
+    }
+    if (command_str == "address") {
       Serial.println("Enter an address (integer 0 to 4095) ex. 0x210 is 528");
       addressFlag = true;
       command_str = "";
-  }
+    }
 }
 
-void send(){
+void sendMessage(){
   CAN.sendMsgBuf(CANaddress, 0, 3, canMsg);
   delay(50);
   for(int i = 0; i<3; i++)    // print the data
@@ -116,41 +104,27 @@ void send(){
   Serial.println("");
   Serial.print("CAN message sent to ID ");
   Serial.println(CANaddress);
-  sendFlag = false;
 }
 
-void edit(){
-  switch (editInt) {
-  case 0:    // your hand is on the sensor
-    canMsg[0] = command_str.toInt() & 0xFF;
-    //c[0] = i & 0xFF;
-    editInt++;
+void editMessage(){ //lol works yay
+  
+    int sentValue = command_str.toInt();
+
+    canMsg[0] = 0x90;
+    canMsg[1] = lowByte(sentValue) & 0xFF;
+    canMsg[2] = highByte(sentValue) & 0xFF;
+    
     command_str = "";
-    Serial.println("Enter the X value: [0,X,0]");
-    break;
-  case 1:    // your hand is close to the sensor
-    canMsg[1] = command_str.toInt() & 0xFF;
-    editInt++;
-    command_str = "";
-    Serial.println("Enter the X value: [0,0,X]");
-    break;
-  case 2:    // your hand is close to the sensor
-    canMsg[2] = command_str.toInt() & 0xFF;
-    editInt=0;
-    command_str = "";
-    Serial.println("Values stored in array for CAN message!");
+    Serial.println(sentValue);
     for(int i = 0; i<3; i++)    // print the data
       {
         Serial.print(canMsg[i]);Serial.print("\t");
       }
-    
+    Serial.println(" were stored in array for CAN message.");
     editFlag = false;
-    
-    break;
-  }
 }
 
-void address(){
+void editAddress(){
   CANaddress = command_str.toInt();
   command_str = "";
   Serial.println("");
@@ -158,25 +132,24 @@ void address(){
   Serial.println(CANaddress);
   addressFlag= false;
 }
-/*
-void pot()
-{
-   // read the value from the sensor:
-    potVal = analogRead(sensorPin);
-    
-    byte high = highByte(potVal);
-    byte low = lowByte(potVal);
 
-    canMsg[0] = 0x90;
-    canMsg[1] = low & 0xFF;
-    canMsg[2] = high & 0xFF;
-    CAN.sendMsgBuf(CANaddress, 0, 3, canMsg);
-    for (int i = 0; i < 3; i++) // print the data
+void continuous(){
+    while(command_str != "stop")
     {
-      Serial.print(canMsg[i]);
-      Serial.print("\t");
+      readSerial();
+      int sentValue = command_str.toInt();
+      canMsg[0] = 0x90;
+      canMsg[1] = lowByte(sentValue) & 0xFF;
+      canMsg[2] = highByte(sentValue) & 0xFF;
+      Serial.println(sentValue);
+      CAN.sendMsgBuf(CANaddress, 0, 3, canMsg);
+      delay(50);
     }
-    delay(50);
-
+    command_str = "";
+    canMsg[0] = 0x90;
+    canMsg[1] = 0;
+    canMsg[2] = 0;
+    CAN.sendMsgBuf(CANaddress, 0, 3, canMsg);
+    Serial.println("STOPPED");
+   
 }
-*/
